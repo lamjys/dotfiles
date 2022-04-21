c6421ffee9e63480b801f189edd20916ec5db920
Joseph Lam <lamjys@gmail.com>
2022-04-21 08:53:57 +0800

    update nmap again

--- bash/nmap-cron.sh
+++ bash/nmap-cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # one liner
-nmap -F -T4 --min-rate 10 192.168.0.142 -oX - | xq -r '.nmaprun.runstats.hosts."@up"' | sed "s/^/,/g" | ts "%Y-%m-%d %H:%M:%S" >> ~/Documents/nmap.log
+nmap -F -T4 --min-rate 10 192.168.0.142 -oX - | xq -r '.nmaprun.runstats.hosts."@up"' | sed "s/^/,/g" | ts "%Y-%m-%d %H:%M:%S" | tee >(mosquitto_pub -h 192.168.0.210 -u lamjys -P $(base64 -d ~/.config/mqtt-base) -t nmap -l) >(tee -a ~/Documents/nmap.log > /dev/null)
+# nmap -F -T4 --min-rate 10 192.168.0.142 -oX - | xq -r '.nmaprun.runstats.hosts."@up"' | sed "s/^/,/g" | ts "%Y-%m-%d %H:%M:%S" >> ~/Documents/nmap.log
 # nmap -F -T4 --min-rate 10 192.168.0.142 -oX /home/nana/Documents/nmap.xml 
 # /home/nana/.local/bin/xq '.nmaprun.runstats.hosts."@up"' /home/nana/Documents/nmap.xml | ts >> /home/nana/Documents/nmap.log
