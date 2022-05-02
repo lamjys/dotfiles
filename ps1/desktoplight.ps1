2092c9d1faa8131c620a8cc0fa4e403b4f3b6793
Joseph Lam <lamjys@gmail.com>
2022-05-02 12:08:41 +0800

    add ignore to bearer sync to git

--- ps1/desktoplight.ps1
+++ ps1/desktoplight.ps1
@@ -7,11 +7,12 @@
 # remove -noexit for powershell window to exit after run
 # set run minimize for the shortcut icon
 
+$bearer = Get-Content .\bearer.txt -Raw
 $param = @{
     Uri           = "http://192.168.0.210:8123/api/services/switch/toggle"
     Method        = "Post"
     Body          = '{"entity_id": "switch.tasmota01"}'
     ContentType   = "application/json"
-    Header        = @{Authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhYTViZTJiZjcyODY0ZmVkYTNkNDU5MjRmMDhkNjk2MyIsImlhdCI6MTY0NDI0MDYwMywiZXhwIjoxOTU5NjAwNjAzfQ.Ggn4RkJM0egCvsXPeNQKKbQGYOYeZJp8NmiwUpXWSIc"}
+    Header        = @{Authorization = $bearer}
 }
 Invoke-RestMethod @param
