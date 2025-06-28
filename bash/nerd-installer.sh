bff96ee64277c6ac98d7c39abdef9ad99eaaa5b4
Joseph Lam <lamjys@gmail.com>
2025-06-28 22:33:43 +0800

    change the nerd fonts download list in nerd-installer.sh

--- bash/nerd-installer.sh
+++ bash/nerd-installer.sh
@@ -33,74 +33,74 @@ else
 fi
 
 declare -a fonts=(
-	0xProto
-	3270
-	Agave
-	AnonymousPro
-	Arimo
-	AurulentSansMono
-	BigBlueTerminal
-	BitstreamVeraSansMono
-	CascadiaCode
-	CascadiaMono
-	CodeNewRoman
-	ComicShannsMono
-	CommitMono
-	Cousine
-	D2Coding
-	DaddyTimeMono
+	# 0xProto
+	# 3270
+	# Agave
+	# AnonymousPro
+	# Arimo
+	# AurulentSansMono
+	# BigBlueTerminal
+	# BitstreamVeraSansMono
+	# CascadiaCode
+	# CascadiaMono
+	# CodeNewRoman
+	# ComicShannsMono
+	# CommitMono
+	# Cousine
+	# D2Coding
+	# DaddyTimeMono
 	DejaVuSansMono
-	DepartureMono
-	DroidSansMono
-	EnvyCodeR
-	FantasqueSansMono
+	# DepartureMono
+	# DroidSansMono
+	# EnvyCodeR
+	# FantasqueSansMono
 	FiraCode
-	FiraMono
-	GeistMono
-	Go-Mono
-	Gohu
-	Hack
-	Hasklig
-	HeavyData
-	Hermit
-	IBMPlexMono
+	# FiraMono
+	# GeistMono
+	# Go-Mono
+	# Gohu
+	# Hack
+	# Hasklig
+	# HeavyData
+	# Hermit
+	# IBMPlexMono
 	Inconsolata
-	InconsolataGo
-	InconsolataLGC
-	IntelOneMono
-	Iosevka
-	IosevkaTerm
-	IosevkaTermSlab
-	JetBrainsMono
-	Lekton
-	LiberationMono
-	Lilex
-	MPlus
-	MartianMono
+	# InconsolataGo
+	# InconsolataLGC
+	# IntelOneMono
+	# Iosevka
+	# IosevkaTerm
+	# IosevkaTermSlab
+	# JetBrainsMono
+	# Lekton
+	# LiberationMono
+	# Lilex
+	# MPlus
+	# MartianMono
 	Meslo
-	Monaspace
-	Monofur
-	Monoid
-	Mononoki
-	NerdFontsSymbolsOnly
-	# Noto
-	OpenDyslexic
-	Overpass
-	ProFont
-	ProggyClean
-	Recursive
-	RobotoMono
-	ShareTechMono
-	SourceCodePro
-	SpaceMono
+	# Monaspace
+	# Monofur
+	# Monoid
+	# Mononoki
+	# NerdFontsSymbolsOnly
+	Noto
+	# OpenDyslexic
+	# Overpass
+	# ProFont
+	# ProggyClean
+	# Recursive
+	# RobotoMono
+	# ShareTechMono
+	# SourceCodePro
+	# SpaceMono
 	Terminus
-	Tinos
-	Ubuntu
+	# Tinos
+	# Ubuntu
 	UbuntuMono
-	UbuntuSans
-	VictorMono
-	ZedMono
-	iA-Writer
+	# UbuntuSans
+	# VictorMono
+	# ZedMono
+	# iA-Writer
 )
 
 if [[ ! -d "$fonts_dir" ]]; then
