7f83767bb9c0e0c64e73a4d40c6a2a3e2f217f08
Joseph Lam <lamjys@gmail.com>
2026-05-31 11:44:20 +0800

    use homeassistant.local for addrees in curl script

--- bash/toggle-kef.sh
+++ bash/toggle-kef.sh
@@ -2,4 +2,4 @@ curl -X POST \
     -H "Authorization: $(base64 -d ~/.config/ha-bearer-base)" \
     -H "Content-Type: application/json" \
     -d '{"entity_id": "media_player.kef"}' \
-    http://192.168.0.210:8123/api/services/media_player/toggle
+    http://homeassistant.local:8123/api/services/media_player/toggle
--- bash/toggle-tasmota.sh
+++ bash/toggle-tasmota.sh
@@ -2,4 +2,4 @@ curl -X POST \
     -H "Authorization: $(base64 -d ~/.config/ha-bearer-base)" \
     -H "Content-Type: application/json" \
     -d '{"entity_id": "switch.tasmota01"}' \
-    http://192.168.0.210:8123/api/services/switch/toggle
+    http://homeassistant.local:8123/api/services/switch/toggle
