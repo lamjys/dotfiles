09b17b5d1a5a0706e55be92a61f1bec5bbf7c306
Joseph Lam <lamjys@gmail.com>
2026-06-06 21:03:28 +0800

    revise ha ip address again instead of mDNS

--- bash/toggle-kef.sh
+++ bash/toggle-kef.sh
@@ -2,4 +2,4 @@ curl -X POST \
     -H "Authorization: $(base64 -d ~/.config/ha-bearer-base)" \
     -H "Content-Type: application/json" \
     -d '{"entity_id": "media_player.kef"}' \
-    http://homeassistant.local:8123/api/services/media_player/toggle
+    http://192.168.0.203:8123/api/services/media_player/toggle
--- bash/toggle-tasmota.sh
+++ bash/toggle-tasmota.sh
@@ -2,4 +2,4 @@ curl -X POST \
     -H "Authorization: $(base64 -d ~/.config/ha-bearer-base)" \
     -H "Content-Type: application/json" \
     -d '{"entity_id": "switch.tasmota01"}' \
-    http://homeassistant.local:8123/api/services/switch/toggle
+    http://192.168.0.203:8123/api/services/switch/toggle
